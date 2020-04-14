c5f02865f38a12547687c237b4a12fdd998449d3
Erik <oceanuheart@gmail.com>
2020-04-15 00:17:14 +0800

    create models.py superadmin

--- survey/mainsite/admin.py
+++ survey/mainsite/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Fill
 
 # Register your models here.
+
+class FillAdmin(admin.ModelAdmin):
+    list_display = ('delivery_date','campaign_id','campaign_name','channel','create_time')
+
+admin.site.register(Fill, FillAdmin)
--- survey/mainsite/models.py
+++ survey/mainsite/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from datetime import date
 
 # Create your models here.
+
+class Fill(models.Model):
+
+    delivery_date = models.DateField(auto_now_add=True)
+    campaign_id = models.CharField(max_length=20)
+    campaign_name = models.CharField(max_length=50)
+    channel = models.CharField(max_length=10)
+    lead_id = models.CharField(max_length=10)
+    units = models.CharField(max_length=4)
+    PM = models.CharField(max_length=10)
+    DA = models.CharField(max_length=10)
+    notes = models.TextField(max_length=100)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-delivery_date','-campaign_id',)
+
+    def __str__(self):
+        return self.campaign_id
